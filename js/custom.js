9b0b819b5df58da999a4bb078530c5fec9de4361
steking1207 <steking1207@gmail.com>
2018-03-07 20:59:18 +0800

    180307

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,14 @@
-//	$().ready(function() {
-//		$("#sn").validate({
-//            submitHandler:function(form){
-//                $("#coupon").fadeIn("fast");
-//            },
-//			errorLabelContainer: $("#sn .error")
-//		});
-//	});
-
-$(".btn-submit").click(function(){
-    $("#coupon").show();
-});
 function myFunction() {
     var copyText = document.getElementById("series");
     copyText.select();
     document.execCommand("Copy");
-//    alert("已複製至剪貼簿: " + copyText.value);
     alert("已複製序號");
 }
+$().ready(function() {
+    $("#sn").validate({
+        submitHandler:function(form){
+            $("#coupon").fadeIn("fast");
+        },
+        errorLabelContainer: $("#sn .error")
+    });
+});

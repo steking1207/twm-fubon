6d65a49978711b04bf2d38f99ce03ec607d14f34
steking1207 <steking1207@gmail.com>
2018-03-07 15:22:42 +0800

    180307

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,15 @@
-	$().ready(function() {
-		$("#sn").validate({
-            submitHandler:function(form){
-        $("#coupon").fadeIn("fast");
-            },
-			errorLabelContainer: $("#sn .error")
-		});
-	});
+//	$().ready(function() {
+//		$("#sn").validate({
+//            submitHandler:function(form){
+//                $("#coupon").fadeIn("fast");
+//            },
+//			errorLabelContainer: $("#sn .error")
+//		});
+//	});
 
+$(".btn-submit").click(function(){
+    $("#coupon").show();
+});
 function myFunction() {
     var copyText = document.getElementById("series");
     copyText.select();
